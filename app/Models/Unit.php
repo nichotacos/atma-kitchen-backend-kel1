8e9366984a63293649fa2845630b77cd8fc69433
Nicholas Yang <210711153@students.uajy.ac.id>
2024-03-21 16:52:54 +0700

    create customer, unit, status, and jenis_ketersediaan models

---
+++ app/Models/Unit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Unit extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id_unit';
+
+    protected $fillable = [
+        'nama_unit'
+    ];
+}
