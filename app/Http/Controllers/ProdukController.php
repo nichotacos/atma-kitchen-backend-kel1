d104864ed70a52720c530c3e25282d21cdd87776
Nicholas Yang <nicho.yeo7@gmail.com>
2024-04-17 15:40:27 +0700

    cruds produk, promopoin, role

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,9 +2,165 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produk;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProdukController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Produk::all();
+        return response([
+            'message' => 'Berhasil menampilkan data',
+            'data' => $products
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'jenis_ketersediaan' => 'required|numeric|between:1,2',
+                'id_ukuran' => 'required|numeric|between:1,5',
+                'id_kategori' => 'required|numeric|between:1,4',
+                'id_kemasan' => 'required|numeric|between:1,6',
+                'id_penitip' => 'required|numeric',
+                'deskripsi' => 'required|string',
+                'harga_produk' => 'required|numeric',
+                'stok' => 'required|numeric',
+                'kuota_harian' => 'required|numeric'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $products = Produk::create([
+                'jenis_ketersediaan' => $request->jenis_ketersediaan,
+                'id_ukuran' => $request->id_ukuran,
+                'id_kategori' => $request->id_kategori,
+                'id_kemasan' => $request->id_kemasan,
+                'id_penitip' => $request->id_penitip,
+                'deskripsi' => $request->deskripsi,
+                'harga_produk' => $request->harga_produk,
+                'stok' => $request->stok,
+                'kuota_harian' => $request->kuota_harian
+            ]);
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Insert Data Success',
+                "data" => $products
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $products = Produk::find($id);
+
+            if (!$products) throw new \Exception("Produk tidak ditemukan!");
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Produk ditemukan',
+                "data" => $products
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function update(Request $request, int $id)
+    {
+        try {
+            $products = Produk::find($id);
+
+            if (!$products) throw new \Exception("Produk tidak ditemukan!");
+
+            $validator = Validator::make($request->all(), [
+                'jenis_ketersediaan' => 'required|numeric|between:1,2',
+                'id_ukuran' => 'required|numeric|between:1,5',
+                'id_kategori' => 'required|numeric|between:1,4',
+                'id_kemasan' => 'required|numeric|between:1,6',
+                'id_penitip' => 'required|numeric',
+                'deskripsi' => 'required|string',
+                'harga_produk' => 'required|numeric',
+                'stok' => 'required|numeric',
+                'kuota_harian' => 'required|numeric'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $products->update($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil update data produk',
+                'data' => $products
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $products = Produk::find($id);
+
+            if (!$products) throw new \Exception("Produk tidak ditemukan!");
+
+            $products->delete();
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil delete produk',
+                "data" => $products
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function search(Request $request)
+    {
+        try {
+            $keyword = $request->input('keyword');
+            $products = Produk::where('nama_produk', 'like', '%' . $keyword . '%')->get();
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil mencari produk',
+                "data" => $products
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
 }
--- app/Http/Controllers/PromoPoinController.php
+++ app/Http/Controllers/PromoPoinController.php
@@ -3,8 +3,139 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PromoPoin;
+use Illuminate\Support\Facades\Validator;
 
 class PromoPoinController extends Controller
 {
-    //
+    public function index()
+    {
+        $promoPoin = PromoPoin::all();
+        return response()->json($promoPoin);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'batas_kelipatan' => 'required|numeric',
+                'poin_diterima' => 'required|integer',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $promoPoin = PromoPoin::create($request->all());
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil menambah promo poin',
+                "data" => $promoPoin
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $promoPoin = PromoPoin::find($id);
+
+            if (!$promoPoin) throw new \Exception("Promo poin tidak ditemukan!");
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Promo poin ditemukan',
+                "data" => $promoPoin
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $promoPoin = PromoPoin::find($id);
+
+            if (!$promoPoin) throw new \Exception("Promo poin tidak ditemukan!");
+
+            $validator = Validator::make($request->all(), [
+                'batas_kelipatan' => 'required|numeric',
+                'poin_diterima' => 'required|integer',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $promoPoin->update($request->all());
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil update promo poin',
+                "data" => $promoPoin
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $promoPoin = PromoPoin::find($id);
+
+            if (!$promoPoin) throw new \Exception("Promo poin tidak ditemukan!");
+
+            $promoPoin->delete();
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil delete promo poin',
+                "data" => $promoPoin
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function search(Request $request)
+    {
+        try {
+            $keyword = $request->input('keyword');
+            $promoPoin = PromoPoin::where('batas_kelipatan', 'LIKE', "%$keyword%")
+                ->orWhere('poin_diterima', 'LIKE', "%$keyword%")
+                ->get();
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil mencari promo poin',
+                "data" => $promoPoin
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
 }
