5f558fe03d6bc8249ff9d3936fba839d9af49841
vinvalen <kvinv0203@gmail.com>
2024-04-22 14:01:53 +0700

    fixed bootstrap

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,27 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl603oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forgot Password</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    
+    <!-- Boostrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+        crossorigin="anonymous">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.18.0/font/bootsrap-icons.css">
+    <style>
+        body {
+            background: #d3d3d3;
+        }
+        .main {
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .form {
+            background: #fff;
+            padding: 50px 30px;
+        }
+    </style>
 </head>
-<style>
-    body {
-        background: #d3d3d3;
-    }
-    .main {
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .form {
-        background: #fff;
-        padding: 50px 30px;
-    }
-</style>
-
 <body>
     <div class="main">
         <div class="form"> 
@@ -44,7 +55,7 @@
             @endif
 
             <h2>Forgot Your Password ?</h2>
-            <p>please enter your mail to password reset request</p>
+            <p>Please enter your mail to password reset request</p>
             <form action="{{ route('password.email') }}" method="post"> 
                 @scrf
                 <label for="email" class="form-label">Email</label>
