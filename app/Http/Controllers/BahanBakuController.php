8a5b4d4d7034696a994852ebde0bbaa841a6fdab
Nicholas Yang <nicho.yeo7@gmail.com>
2024-05-08 09:32:47 +0700

    test

--- app/Http/Controllers/BahanBakuController.php
+++ app/Http/Controllers/BahanBakuController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\BahanBaku;
-use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BahanBakuController extends Controller
 {
+
     public function index(Request $request)
     {
         try {
@@ -16,9 +18,9 @@ class BahanBakuController extends Controller
             if ($request->search) {
                 $bahanBakus->where(function ($query) use ($request) {
                     $query->where('nama_bahan_baku', 'like', '%' . $request->search . '%')
-                          ->orWhereHas('unit', function (Builder $query) use ($request) {
-                              $query->where('nama_unit', 'like', '%' . $request->search . '%');
-                          });
+                        ->orWhereHas('unit', function (Builder $query) use ($request) {
+                            $query->where('nama_unit', 'like', '%' . $request->search . '%');
+                        });
                 });
             }
 
@@ -44,6 +46,132 @@ class BahanBakuController extends Controller
                 'status' => true,
                 'message' => 'Berhasil menampilkan data bahan baku',
                 'data' => $data
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+
+    public function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'nama_bahan_baku' => 'required|string',
+                'stok_bahan_baku' => 'required|numeric'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $bahan_bakus = BahanBaku::create([
+                'nama_bahan_baku' => $request->nama_bahan_baku,
+                'stok_bahan_baku' => $request->stok_bahan_baku
+            ]);
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Insert Data Success',
+                "data" => $bahan_bakus
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $bahan_bakus = BahanBaku::find($id);
+
+            if (!$bahan_bakus) throw new \Exception("Bahan Baku tidak tersedia!");
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Bahan baku tersedia',
+                "data" => $bahan_bakus
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function update(Request $request, int $id)
+    {
+        try {
+            $bahan_bakus = BahanBaku::find($id);
+
+            if (!$bahan_bakus) throw new \Exception("Bahan Baku tidak tersedia!");
+
+            $validator = Validator::make($request->all(), [
+                'nama_bahan_baku' => 'required|string',
+                'stok_bahan_baku' => 'required|numeric'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $bahan_bakus->update($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil update data bahan baku',
+                'data' => $bahan_bakus
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $bahan_bakus = BahanBaku::find($id);
+
+            if (!$bahan_bakus) throw new \Exception("Bahan Baku tidak tersedia!");
+
+            $bahan_bakus->delete();
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil delete bahan baku',
+                "data" => $bahan_bakus
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ], 400);
+        }
+    }
+
+    public function search($keyword)
+    {
+        try {
+            $bahan_bakus = BahanBaku::where('nama_bahan_baku', 'like', '%' . $keyword . '%')->get();
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil mencari bahan baku',
+                "data" => $bahan_bakus
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
